ebb96f666538f43f4cab00a032ca2aa260f67f6b
STBenji <stevenbenjumea9@gmail.com>
2024-05-28 20:28:02 -0500

    added entities and connection to the database

---
+++ BookApi/Domain/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookApi.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required string Gender { get; set; }
+        public required string Language { get; set; }
+
+    }
+}
