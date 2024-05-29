9ca1aeaab0b64cf315844de4a2809f517043dc1b
STBenji <stevenbenjumea9@gmail.com>
2024-05-28 20:29:19 -0500

    created interface and CRUD operations file

---
+++ BookApi/Data/Repositories/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using BookApi.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApi.Data.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BookRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book> GetById(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task InsertBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBook(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
---
+++ BookApi/Data/Repositories/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using BookApi.Domain;
+
+namespace BookApi.Data.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllBooks();
+        Task<Book> GetById(int id);
+        Task InsertBook(Book book);
+        Task UpdateBook(Book book);
+        Task DeleteBook(int id);
+    }
+}
